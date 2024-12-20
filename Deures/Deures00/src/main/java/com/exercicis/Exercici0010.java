65f8c21880d574bcaabc9a7cb2f0004a2d421e6c
kyufle <rromerocarretero.cf@iesesteveterradas.cat>
2024-12-20 23:11:58 +0100

    exercici 009 fet

--- Deures/Deures00/src/main/java/com/exercicis/Exercici0010.java
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0010.java
@@ -6,17 +6,20 @@ import java.util.Random;
 public class Exercici0010 {
 
     public static ArrayList<Integer> generarNumerosAleatoris(int quantitat, int min, int max) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        Random random = new Random();
+        ArrayList<Integer> arr =new ArrayList<>();
+        for (int i = 0; i < quantitat;i++){
+            arr.add(random.nextInt(min,max));
+        }
+        return arr;
     }
 
     public static ArrayList<Integer> multiplicarPerDos(ArrayList<Integer> llista) {
-        /*
-            TODO: Resol aquí la funció
-        */
-        return new ArrayList<>();
+        ArrayList<Integer>resultatmultiplicar= new ArrayList<>();
+        for (int numbers : resultatmultiplicar){
+            resultatmultiplicar.add((numbers * 2));
+        }
+        return resultatmultiplicar;
     }
 
     public static ArrayList<Integer> filtrarImparells(ArrayList<Integer> llista) {
