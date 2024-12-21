4e4dc8ea5c6811a578463b5320954e4b9fd60c87
kyufle <rromerocarretero.cf@iesesteveterradas.cat>
2024-12-21 03:58:12 +0100

    exercici 0006 corregit

--- Deures/Deures00/src/main/java/com/exercicis/Exercici0006.java
+++ Deures/Deures00/src/main/java/com/exercicis/Exercici0006.java
@@ -27,10 +27,10 @@ public class Exercici0006 {
     public static void main(String[] args) {
         String template = "%-25s%.2f€";
         System.out.println(String.format(Locale.US,template,"Cas 1 (2 adults, 2 nens, 1 gran):  ",calculaEntrada(2,2, 1, "dimarts")));
-        System.out.println(String.format(Locale.US,template,"Cas 2 (1 adult,  2 nens, 0 grans):  ",calculaEntrada(1,2, 0, "dijous")));
-        System.out.println(String.format(Locale.US,template,"Cas 3 (0 adults, 0 nens, 2 grans):  ",calculaEntrada(0,0,2, "dissabte")));
-        System.out.println(String.format(Locale.US,template,"Cas 4 (1 adult,  0 nens, 0 grans):  ",calculaEntrada(1, 0, 0, "dimenge")));
-        System.out.println(String.format(Locale.US,template,"Cas 5 (1 adult,  3 nens, 4 grans):  ",calculaEntrada(1, 3, 4, "dijous")));
+        System.out.println(String.format(Locale.US,template,"Cas 2 (1 adult,  2 nens, 0 grans): ",calculaEntrada(1,2, 0, "dijous")));
+        System.out.println(String.format(Locale.US,template,"Cas 3 (0 adults, 0 nens, 2 grans): ",calculaEntrada(0,0,2, "dissabte")));
+        System.out.println(String.format(Locale.US,template,"Cas 4 (1 adult,  0 nens, 0 grans): ",calculaEntrada(1, 0, 0, "dimenge")));
+        System.out.println(String.format(Locale.US,template,"Cas 5 (1 adult,  3 nens, 4 grans): ",calculaEntrada(1, 3, 4, "dijous")));
         System.out.println(String.format(Locale.US,template,"Cas 6 (2 adults, 2 nens, 1 gran):  ",calculaEntrada(2, 2, 1, "diumenge")));
     }
 }
